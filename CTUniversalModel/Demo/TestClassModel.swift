e9229b0f443364973beedd67364aa38f614f4357
Casa Taloyum <casatwy@msn.com>
2020-03-21 19:47:55 +0800

    modification

---
+++ CTUniversalModel/Demo/TestClassModel.swift
@@ -0,0 +1,13 @@
+//
+//  TestClassModel.swift
+//  CTUniversalModel
+//
+//  Created by casa on 2020/3/21.
+//  Copyright © 2020 casa. All rights reserved.
+//
+
+import Foundation
+
+class TestClassModel {
+    var name:String?
+}
---
+++ CTUniversalModel/Demo/TestStructModel.swift
@@ -0,0 +1,13 @@
+//
+//  TestStructModel.swift
+//  CTUniversalModel
+//
+//  Created by casa on 2020/3/21.
+//  Copyright © 2020 casa. All rights reserved.
+//
+
+import Foundation
+
+struct TestStructModel {
+    var name:String?
+}
